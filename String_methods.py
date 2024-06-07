5c3c4e99fec6f8cbf718675f3f09befa78de6480
tea551234 <tea551234@gmail.com>
2024-06-07 17:42:01 +0800

    11

---
+++ String_methods.py
@@ -0,0 +1,34 @@
+f1=1.32
+print("%3.1f" %f1)
+
+
+# str_a = "abc"
+# str_b = "def"
+
+# str_c = str_a[-3:-1]
+# unicode_numbers = [21830, 31649, 31243, 24335, 35373, 35336]
+# unicode_string = ''.join(chr(num) for num in unicode_numbers)
+# print(unicode_string)
+
+# # String methods
+# s = "athletes could not join the parade"
+
+# #  capitalize 
+# print("capitalize : " +s.capitalize()) # 將字串的第一個字母轉為大寫
+# # title 
+# print(  "title : "+ s.title()) # 將字串中的每個單字的第一個字母轉為大寫
+# # upper
+# print("upper : " + s.upper()) # 將字串中的所有字母轉為大寫
+# # replace
+# print("replace : " + s.replace("join","~~~TEA~~~")) 
+# # count
+# print(s.count("e")) # 計算字串中有幾個指定的字元
+# # in 
+# print("join" in s) # 判斷字串中是否有指定的字串
+# #find
+# print(s.find("join")) # 找出字串中指定字串的位置
+# #split
+# print(s.split(" ")) # 將字串分割成一個list
+# #strip
+# print(s.strip()) # 移除字串前後的空白
+# print(s.strip("a")) # 移除字串中指定的字元
---
+++ W2.py
@@ -0,0 +1,10 @@
+# tuple immutable object
+
+mytuple = (1, 2, 3)
+mytuple = mytuple + (-1,) # 透過串接的方式,賦予tuple
+print(mytuple)
+
+# map operator 
+# map(function,iterable(array,tuple,list,dict)) -> tuple object
+
+list1 = [3.5,1,2,12]
